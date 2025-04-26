3d41dee942527a11dd4ff2ff8ee282a0f9a08fd1
devicoder <67104170+devicoder@users.noreply.github.com>
2025-04-26 15:43:59 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore}from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxh-6lUsBnleQQwjirmLdx7Vouqsfiel0",
-  authDomain: "hackathon2025-55f41.firebaseapp.com",
-  projectId: "hackathon2025-55f41",
-  storageBucket: "hackathon2025-55f41.firebasestorage.app",
-  messagingSenderId: "376132306989",
-  appId: "1:376132306989:web:1eeecb841bd44ac1ec10a4",
-  measurementId: "G-XRC2GTJCJ8"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket:
+  messagingSenderId: 
+  appId:
+  measurementId: 
 };
 
 // Initialize Firebase
